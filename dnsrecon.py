42a7767047494f247df2cf738655a6eeb3f6ff1e
sealmindset <sealmindset@gmail.com>
2016-03-21 17:24:24 -0500

    Update dnsrecon.py

--- dnsrecon.py
+++ dnsrecon.py
@@ -3,6 +3,12 @@ import subprocess
 import sys
 import reconf
 from reconf import *
+import dns.name
+import dns.query
+import dns.dnssec
+import dns.message
+import dns.resolver
+import dns.rdatatype
 
 if len(sys.argv) != 2:
     print "Usage: dnsrecon.py <ip address>"
@@ -24,3 +30,36 @@ else:
     dnsf.write(ztresults)
     dnsf.close
 
+
+
+# get nameservers for target domain
+response = dns.resolver.query('example.com.',dns.rdatatype.NS)
+
+# we'll use the first nameserver in this example
+nsname = response.rrset[0] # name
+response = dns.resolver.query(nsname,dns.rdatatype.A)
+nsaddr = response.rrset[0].to_text() # IPv4
+
+# get DNSKEY for zone
+request = dns.message.make_query('example.com.',
+                                 dns.rdatatype.DNSKEY,
+                                 want_dnssec=True)
+
+# send the query
+response = dns.query.udp(request,nsaddr)
+if response.rcode() != 0:
+    # HANDLE QUERY FAILED (SERVER ERROR OR NO DNSKEY RECORD)
+
+# answer should contain two RRSET: DNSKEY and RRSIG(DNSKEY)
+answer = response.answer
+if len(answer) != 2:
+    # SOMETHING WENT WRONG
+
+# the DNSKEY should be self signed, validate it
+name = dns.name.from_text('example.com.')
+try:
+    dns.dnssec.validate(answer[0],answer[1],{name:answer[0]})
+except dns.dnssec.ValidationFailure:
+    # BE SUSPICIOUS
+else:
+    # WE'RE GOOD, THERE'S A VALID DNSSEC SELF-SIGNED KEY FOR example.com
