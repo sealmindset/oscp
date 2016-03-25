121144e88ce9b369a168c13c9fe0df7c5787b759
sealmindset <sealmindset@gmail.com>
2016-03-24 19:38:18 -0500

    Update f1.py

--- f1.py
+++ f1.py
@@ -3,41 +3,14 @@
 import ftplib
 import os
 
-from ftplib import FTP
+import ftputil.ftp_error.PermanentError as PermanentError
 
-def get_dirs_ftp(folder=""):
-    contents = ftp.nlst(folder)
-    folders = []
-    for item in contents:
-        if "." not in item:
-            folders.append(item)
-    return folders
-
-def get_all_dirs_ftp(folder=""):
-    dirs = []
-    new_dirs = []
-
-    new_dirs = get_dirs_ftp(folder)
-
-    while len(new_dirs) > 0:
-        for dir in new_dirs:
-            dirs.append(dir)
-
-        old_dirs = new_dirs[:]
-        new_dirs = []
-        for dir in old_dirs:
-            for new_dir in get_dirs_ftp(dir):
-                new_dirs.append(new_dir)
-
-    dirs.sort()
-    return dirs
-
-
-host ="your host"
-user = "user"
-password = "password"
-
-print("Connecting to {}".format(host))
-ftp = FTP(host)
-ftp.login(user, password)
-print("Connected to {}".format(host))
+ftp = ftputil.FTPHost('ftp.site.com','user','pass')
+try:
+    recursive = ftp.walk("/path/dir1",topdown=True,onerror=None)
+    for root,dirs,files in recursive:
+         for name in files:
+              print name
+except PermanentError e:
+   print "Permanent Error: %s occurred" % (e)
+ftp.close
